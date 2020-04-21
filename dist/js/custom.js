04d5b433c429cd5eecf4b7bc3bce4fb66ef93f02
vhk <vladkukhlii"gmail.com>
2020-04-21 21:11:36 +0300

    burger bugs fix

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -5,7 +5,7 @@
  *
  */
 var vWidth = window.innerWidth;
-
+alert(window);
 (function () {
     var articles = document.querySelector('.articles');
     var cardsslider = document.querySelectorAll('[data-cardslider]');
