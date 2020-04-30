795338ccf2f3226e99b089c4acaf6adfaf8be126
vhk <vladkukhlii@gmail.com>
2020-04-30 19:41:14 +0300

    item color fix

--- dist/js/classListPolyfill.js
+++ dist/js/classListPolyfill.js
@@ -90,3 +90,16 @@ if (!Element.prototype.closest) {
         return null;
     };
 }
+if (!window.Element || !window.Element.prototype || !window.Element.prototype.hasAttribute) {
+
+    (function () {
+        function hasAttribute (attrName) {
+            return typeof this[attrName] !== 'undefined'; // You may also be able to check getAttribute() against null, though it is possible this could cause problems for any older browsers (if any) which followed the old DOM3 way of returning the empty string for an empty string (yet did not possess hasAttribute as per our checks above). See https://developer.mozilla.org/en-US/docs/Web/API/Element.getAttribute
+        }
+        var inputs = document.getElementsByTagName('input');
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].hasAttribute = hasAttribute;
+        }
+    }());
+
+}
